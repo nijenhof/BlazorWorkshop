01ed2e819cd4e9f9f1afe829c954bc0a82863497
killermi200 <killermi200@gmail.com>
2022-01-05 12:33:01 +0100

    Update WeatherForecastService.cs

--- BlazorWorkshop/Client/Services/Implementations/WeatherForecastService.cs
+++ BlazorWorkshop/Client/Services/Implementations/WeatherForecastService.cs
@@ -1,6 +1,31 @@
-﻿namespace BlazorWorkshop.Client.Services.Implementations
+﻿using BlazorWorkshop.Client.Services.Interfaces;
+using BlazorWorkshop.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorWorkshop.Client.Services.Implementations
 {
-    public class WeatherForecastService
+    public class WeatherForecastService : IWeatherForecastService
     {
+        private readonly HttpClient _httpClient;
+        public WeatherForecastService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddForecast(WeatherForecast newForecast)
+        {
+            await _httpClient.PostAsJsonAsync("WeatherForecast", newForecast);
+        }
+
+        public async Task<IEnumerable<WeatherForecast>> GetAll()
+        {
+            var result = await _httpClient.GetFromJsonAsync<IEnumerable<WeatherForecast>>("WeatherForecast");
+
+            if (result == null)
+            {
+                return Enumerable.Empty<WeatherForecast>();
+            }
+            return result;
+        }
     }
 }
